f0101824338ccbd73a08d9a54257755ad03e3655
Sroge91 <64093136+Sroge91@users.noreply.github.com>
2020-09-22 09:08:13 +0200

    Update yams.java

--- yams.java
+++ yams.java
@@ -2,21 +2,24 @@ public class yams {
     public static void main(String[] args){
 
       System.out.println("---YAMS---");
-      lancer(5);
+      lancer(7);
     }
     
-    public static int[] lancer(int n){
+    public static int[] lancer(int n){ //Lancé de n dés
       if (n>5){
         n=5;
       }
+
     int dés_lancés[] = new int[n];
     for (int i=0; i<n; i++){
       dés_lancés[i] = 1 + (int)(Math.random() * ((6 - 1) + 1));
     }
+
     System.out.println(n + " dés lancés.");
     for (int i=0; i<n; i++){
       System.out.println(dés_lancés[i]);
     }
+
     return dés_lancés;
   }
   }
